3db2a3752335cae4f66307fdb2fdced153c2210d
B Kleinen <kleinen@infrastructure.de>
2011-10-11 12:58:21 +0200

    fixed syntax

--- ruby-01/Apple.rb
+++ ruby-01/Apple.rb
@@ -3,9 +3,9 @@ class Apple
   attr_accessor :color
   @@number_of_apples = 0
 
-  def initialize(color)
+  def initialize(color = "red")
     @color = color if color
-    @@number_of_app(Please provide a comma-separated list)les += 1
+    @@number_of_apples += 1
   end
   def Apple.count
     @@number_of_apples
@@ -20,8 +20,11 @@ end
 
 
 a1 = Apple.new("red")
-a1.insta
+puts Apple.count
 a2 = Apple.new("green")
 puts a2.to_s
 puts "There are #{Apple.count} apples."
 puts "This apple is #{a1.color}."
+a1.color = "pink"
+puts "This apple is #{a1.color}."
+puts a1.instance_variables
